c82f6c226b1aac27d5bfbb54a003300ae79403ab
Yasser Omar Jammeli <yasser.omar.jammeli@gmail.com>
2023-07-17 23:12:27 +0200

    update constants

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -2,14 +2,12 @@ const String baseUrl = 'http://api.weatherstack.com/';
 const String accessKeyValue = '8c6649952c96f273713a776d91740ff5';
 const Duration timeOutDuration = Duration(seconds: 20);
 
-
+//app keys
 const String accessKey = 'access_key';
+const String userCitiesKey = 'user_cities';
+const String userMetricsKey = 'user_metrics';
+const String celsiusKey = 'celsius';
+const String fahrenheitKey = 'fahrenheit';
 
-
+// app uris
 const String currentWeatherUri = 'current';
-
-
-
-
-
-
