3b334eb8167c8f833093e64ddf74f3b2ec9ae93d
Yasser Omar Jammeli <yasser.omar.jammeli@gmail.com>
2023-07-17 23:15:39 +0200

    add multi metrics support

--- lib/modules/weather_information/data/data_source/weather_remote_data_source_impl.dart
+++ lib/modules/weather_information/data/data_source/weather_remote_data_source_impl.dart
@@ -1,7 +1,10 @@
 import 'dart:convert';
 
 import 'package:flaconi_case_study/core/constants/app_constants.dart';
+import 'package:flaconi_case_study/core/enums/weather_metrics_enum.dart';
+import 'package:flaconi_case_study/core/managers/user_manager.dart';
 import 'package:flaconi_case_study/core/service/base_service.dart';
+import 'package:flaconi_case_study/core/utils/dependency_injection/component/app_component.dart';
 import 'package:flaconi_case_study/modules/weather_information/data/data_source/weather_remote_data_source.dart';
 import 'package:flaconi_case_study/modules/weather_information/data/model/weather_response_model.dart';
 import 'package:http/http.dart' as http;
@@ -20,6 +23,7 @@ class WeatherRemoteDataSourceImpl implements WeatherRemoteDataSource {
         uri: currentWeatherUri,
         params: {
           'query': city,
+          'units': _getUserUnits(),
         },
       );
       final dynamic decodedJson = jsonDecode(result.body);
@@ -28,6 +32,17 @@ class WeatherRemoteDataSourceImpl implements WeatherRemoteDataSource {
       return responseModel;
     } catch (e) {
       throw Exception();
+    }
+  }
+
+  String _getUserUnits() {
+    switch (locator<UserManager>().userMetric) {
+      case WeatherMetricsEnum.celsius:
+        return 'm';
+      case WeatherMetricsEnum.fahrenheit:
+        return 'f';
+      default:
+        return 'm';
     }
   }
 }
